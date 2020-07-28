f640ef4904d50f042acf0da0a3457fc9bf32dbd2
Vikramaditya <vik.suryawanshi@gmail.com>
2020-07-28 19:38:27 +0530

    Added App api constants in the file. This APIConstants file is for adding all api constant for global access.

---
+++ Tech-M-Test1/Constants/APIConstants.swift
@@ -0,0 +1,12 @@
+//
+//  APIConstants.swift
+//  Tech-M-Test1
+//
+//  Created by Apps on 26/07/20.
+//  Copyright © 2020 Vikramaditya. All rights reserved.
+//
+
+import Foundation
+struct APIConstants {
+    static let getFactsUrl = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
+}
